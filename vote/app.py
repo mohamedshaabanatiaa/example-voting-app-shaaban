58a5c6b13b4491c8c72593e97163ef5e56a1e159
mohamedshaabanatiaa <100875782+mohamedshaabanatiaa@users.noreply.github.com>
2022-03-06 15:06:09 +0400

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -5,6 +5,10 @@ import socket
 import random
 import json
 import logging
+import statsd
+c = statsd.StatsClient('localhost', 8125)
+c.incr('foo')  # Increment the 'foo' counter.
+c.timing('stats.timed', 320)  # Record a 320ms 'stats.timed'.
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
@@ -49,3 +53,5 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    
+
