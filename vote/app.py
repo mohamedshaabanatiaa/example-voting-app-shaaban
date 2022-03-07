83eecaefc2b4c178176b4219a1788b4ea48f248b
mohamedshaabanatiaa <100875782+mohamedshaabanatiaa@users.noreply.github.com>
2022-03-07 12:35:38 +0400

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 import statsd
 c = statsd.StatsClient('localhost', 8125)
-c.incr('foo')  # Increment the 'foo' counter.
+c.incr('foo')  ## Increment the 'foo' counter.
 c.timing('stats.timed', 320)  # Record a 320ms 'stats.timed'.
 
 option_a = os.getenv('OPTION_A', "Cats")
